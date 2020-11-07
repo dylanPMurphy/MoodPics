8a9d9851d1202873018e0c32517ad1ad7f853e1b
dylanPMurphy <dylanmurphydba@gmail.com>
2020-11-06 19:53:11 -0800

    nice hover css goin on

--- moodpics/moodpics_app/views.py
+++ moodpics/moodpics_app/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request){
-    return HttpResponse("You CAN DO IT")
-}
+def index(request):
+    return render(request, 'index.html')

53d491f15c6690e03acd57bdd430947242cad3cf
mac <dylanmurphyDBA@gmail.com>
2020-12-02 23:28:05 -0800

    added some admin features

--- moodpics/moodpics_app/admin.py
+++ moodpics/moodpics_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import * # add this
+from login_reg.models import *
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):  # add this
+    list_display = ('username', 'email', 'password') # add this
+
+admin.site.register(User, UserAdmin)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'img', 'mood', 'poster')
+admin.site.register(Post, PostAdmin)
